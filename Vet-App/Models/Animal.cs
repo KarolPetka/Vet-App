a3dbe170007d9316481d621e5f46167ee53f5df6
Karol Petka <karolpetka1@gmail.com>
2022-04-24 19:48:26 +0200

    add fields to Animal model class

--- Vet-App/Models/Animal.cs
+++ Vet-App/Models/Animal.cs
@@ -1,6 +1,16 @@
-﻿namespace Vet_App.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vet_App.Models
 {
     public class Animal
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Specie { get; set; }
+        public string VisitCause { get; set; }
+        public string OwnerName { get; set; }
     }
 }
