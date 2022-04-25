e9c503c1bf131676978c773f298a83cf4e1db38d
Karol Petka <karolpetka1@gmail.com>
2022-04-25 21:35:38 +0200

    implement DbContext to AnimalDatabaseContext class

--- Vet-App/Context/AnimalDatabaseContext.cs
+++ Vet-App/Context/AnimalDatabaseContext.cs
@@ -1,7 +1,12 @@
-﻿namespace Vet_App.Context
-{
-    public class AnimalDatabaseContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using Vet_App.Models;
 
+namespace Vet_App.Context
+{
+    public class AnimalDatabaseContext : DbContext
+    {
+        public AnimalDatabaseContext(DbContextOptions<AnimalDatabaseContext> options) : base(options) { }
+
+        public DbSet<Animal> Animals { get; set; }
     }
 }
