dadbd30e0af0013c4106f03af2b6188804b376b2
Karol Petka <karolpetka1@gmail.com>
2022-04-30 11:22:36 +0200

    inject database context

--- Vet-App/Services/AnimalsService.cs
+++ Vet-App/Services/AnimalsService.cs
@@ -1,6 +1,14 @@
-﻿namespace Vet_App.Services
+﻿using Vet_App.Context;
+
+namespace Vet_App.Services
 {
     public class AnimalsService
     {
+        private readonly AnimalDatabaseContext _animalDatabaseContext;
+
+        public AnimalsService(AnimalDatabaseContext animalDatabaseContext)
+        {
+            _animalDatabaseContext = animalDatabaseContext;
+        }
     }
 }
